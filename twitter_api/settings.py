eaa579aaafb7101d3253bad6e7515445a54a6682
Mumin <r.muminur@gmail.com>
2022-03-27 02:27:54 +0600

    Add postgres settings

--- twitter_api/settings.py
+++ twitter_api/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = "twitter_api.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twitter_db',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'postgres_container',
+        'PORT': '5432',
+    },
+
 }
 
 # Password validation
@@ -155,8 +160,8 @@ LOGGING = {
     "formatters": {
         "console": {
             "format": "%(levelname)s | %(asctime)s | %(filename)s |"
-            " %(module)s:%(funcName)s:%(lineno)d |"
-            " %(message)s",
+                      " %(module)s:%(funcName)s:%(lineno)d |"
+                      " %(message)s",
             "datefmt": "%Y-%m-%d %H:%M:%S",
         },
     },
