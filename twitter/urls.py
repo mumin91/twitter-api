53b77699d3ca7d73038062d662ffd24d0dd00183
Mumin <r.muminur@gmail.com>
2022-03-27 03:24:26 +0600

    Fix follow url

--- twitter/urls.py
+++ twitter/urls.py
@@ -5,7 +5,7 @@ from twitter.views import *
 
 urlpatterns = [
     path("login/", views.obtain_auth_token, name="login"),
-    path("follow/unfollow/", FollowView.as_view(), name="follow_unfollow_user"),
+    path("follow/", FollowView.as_view(), name="follow_unfollow_user"),
     path("tweet/", TweetView.as_view(), name="tweet"),
     path("me/tweets/", MyTweetsView.as_view(), name="my_tweets"),
     path("me/newsfeed/", NewsfeedView.as_view(), name="newsfeed"),
